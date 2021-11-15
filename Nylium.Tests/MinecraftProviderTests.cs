d28754fc780bcc7c40b768af2abeaa07edc06d1c
Yucked <yucked@pm.me>
2021-11-15 16:38:09 -0500

    added unit tests for quick debugging

---
+++ Nylium.Tests/MinecraftProviderTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nylium.Minecraft.Providers;
+using System.Threading.Tasks;
+
+namespace Nylium.Tests;
+
+[TestClass]
+public sealed class MinecraftProviderTests {
+
+    [TestMethod]
+    public async Task FabricReleasesTest() {
+        var provider = Statics.GetService<FabricProvider>();
+        var releases = await provider.GetReleasesAsync();
+        Assert.IsNotNull(releases);
+    }
+}
---
+++ Nylium.Tests/Statics.cs
@@ -0,0 +1,20 @@
+using Microsoft.Extensions.DependencyInjection;
+using Nylium.Minecraft.Providers;
+using System;
+using System.Net.Http;
+
+namespace Nylium.Tests;
+internal readonly record struct Statics {
+
+    public static IServiceProvider Provider
+        => new ServiceCollection()
+        .AddLogging()
+        .AddSingleton<HttpClient>()
+        .AddSingleton<FabricProvider>()
+        .BuildServiceProvider();
+
+    public static T GetService<T>() {
+#pragma warning disable CS8714
+        return Provider.GetRequiredService<T>();
+    }
+}
