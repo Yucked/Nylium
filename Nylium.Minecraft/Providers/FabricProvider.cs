de36f798180e29e91f2021defef7431eef6c6942
Yucked <yucked@pm.me>
2021-12-12 20:13:01 -0500

    Move FabricProvider.cs logic to Fabric wrapper

--- Nylium.Minecraft/Providers/FabricProvider.cs
+++ Nylium.Minecraft/Providers/FabricProvider.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Logging;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+
 // ReSharper disable UnusedAutoPropertyAccessor.Local
 
 namespace Nylium.Minecraft.Providers;
@@ -9,7 +10,7 @@ namespace Nylium.Minecraft.Providers;
 public sealed class FabricProvider : IServerProvider {
     /// <inheritdoc />
     public string Url
-        => "https://api.github.com/repos/FabricMC/fabric/releases";
+        => "https://meta.fabricmc.net/v2/versions";
 
     private readonly HttpClient _httpClient;
     private readonly ILogger<FabricProvider> _logger;
@@ -26,40 +27,59 @@ public sealed class FabricProvider : IServerProvider {
 
     /// <inheritdoc />
     public async ValueTask<IEnumerable<Release>?> GetReleasesAsync() {
-        using var requestMessage = new HttpRequestMessage(HttpMethod.Get, Url)
-            .WithBrowserHeader();
-        using var responseMessage = await _httpClient.SendAsync(requestMessage);
+        var versions = await GetMinecraftVersions();
 
+        return default;
+    }
+
+    private async Task<IEnumerable<Version>> GetMinecraftVersions() {
+        await using var stream = await GetStreamAsync("game");
+        if (stream == null || stream.Length == 0) {
+            _logger.LogError("Fetching minecraft versions failed!");
+            return default!;
+        }
+
+        using var document = await JsonDocument.ParseAsync(stream);
+        return document.RootElement.EnumerateArray()
+            .Select(x => new Version {
+                Minecraft = x.GetProperty("version").GetString()!,
+                IsPrerelease = x.GetProperty("stable").GetBoolean()
+            })!;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="version"></param>
+    public async Task GetVersionLoaders(string version) {
+        await using var stream = await GetStreamAsync($"loader/{version}");
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public async Task<IEnumerable<string>> GetInstallers() {
+        await using var stream = await GetStreamAsync("installer");
+        if (stream == null || stream.Length == 0) {
+            _logger.LogError("Fetching minecraft versions failed!");
+            return default!;
+        }
+
+        using var document = await JsonDocument.ParseAsync(stream);
+        return document.RootElement.EnumerateArray()
+            .Select(x => x.GetProperty("version").GetString()!);
+    }
+
+    private async Task<Stream?> GetStreamAsync(string path) {
+        using var requestMessage = new HttpRequestMessage(HttpMethod.Get, $"{Url}/{path}");
+        using var responseMessage = await _httpClient.SendAsync(requestMessage);
         if (!responseMessage.IsSuccessStatusCode) {
             _logger.LogError("{ReasonPhrase}", responseMessage.ReasonPhrase);
             return default;
         }
 
         using var content = responseMessage.Content;
-        await using var stream = await content.ReadAsStreamAsync();
-
-        var gitHubReleases = await JsonSerializer.DeserializeAsync<IEnumerable<GitHubRelease>>(stream);
-        return gitHubReleases!.Select(release => new Release {
-            DownloadUrl = release.Url,
-            ReleasedOn = release.CreatedOn,
-            Version = new Version(release.Target, release.Tag, release.IsPrerelease)
-        });
-    }
-
-    private readonly record struct GitHubRelease {
-        [JsonPropertyName("browser_download_url")]
-        public string Url { get; init; }
-
-        [JsonPropertyName("tag_name")]
-        public string Tag { get; init; }
-
-        [JsonPropertyName("target_commitish")]
-        public string Target { get; init; }
-
-        [JsonPropertyName("created_at")]
-        public DateTimeOffset CreatedOn { get; init; }
-
-        [JsonPropertyName("prerelease")]
-        public bool IsPrerelease { get; init; }
+        return await content.ReadAsStreamAsync();
     }
 }
