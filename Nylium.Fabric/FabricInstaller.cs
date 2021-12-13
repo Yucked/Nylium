2cfee83619b2785fc9c04135b9db4ba0c417ebae
Yucked <yucked@pm.me>
2021-12-12 20:11:36 -0500

    Cover Fabric's API

---
+++ Nylium.Fabric/FabricInstaller.cs
@@ -0,0 +1,10 @@
+﻿namespace Nylium.Fabric;
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="Url"></param>
+/// <param name="Maven"></param>
+/// <param name="Version"></param>
+/// <param name="IsStable"></param>
+public readonly record struct FabricInstaller(string Url, string Maven, string Version, bool IsStable);
---
+++ Nylium.Fabric/FabricLoader.cs
@@ -0,0 +1,11 @@
+﻿namespace Nylium.Fabric;
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="Seperator"></param>
+/// <param name="Build"></param>
+/// <param name="Maven"></param>
+/// <param name="Version"></param>
+/// <param name="IsStable"></param>
+public readonly record struct FabricLoader(char Seperator, int Build, string Maven, string Version, bool IsStable);
