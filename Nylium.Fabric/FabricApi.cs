ea33d19a75fcb24f7b3ab06c2ff511d5839e8d1a
Yucked <yucked@pm.me>
2021-12-13 17:01:40 -0500

    cleaned up fabric api

--- Nylium.Fabric/FabricApi.cs
+++ Nylium.Fabric/FabricApi.cs
@@ -1,5 +1,6 @@
-﻿using System.Text.Json;
+using System.Text.Json;
 using Microsoft.Extensions.Logging;
+using Nylium.Common;
 using Nylium.Fabric.Responses;
 
 namespace Nylium.Fabric;
@@ -35,7 +36,7 @@ public sealed class FabricApi {
     /// </summary>
     /// <returns></returns>
     public Task<IReadOnlyList<FabricLoader>?> GetLoadersAsync() {
-        return GetJsonAsync<IReadOnlyList<FabricLoader>>("loader");
+        return _httpClient.GetJsonAsync<IReadOnlyList<FabricLoader>>($"{META_BASE_URL}/loader", Defaults);
     }
 
     /// <summary>
@@ -44,7 +45,7 @@ public sealed class FabricApi {
     /// <param name="gameVersion"></param>
     /// <returns></returns>
     public Task<IReadOnlyList<FabricGameLoader>?> GetLoadersAsync(string gameVersion) {
-        return GetJsonAsync<IReadOnlyList<FabricGameLoader>>($"loader/{gameVersion}");
+        return _httpClient.GetJsonAsync<IReadOnlyList<FabricGameLoader>>($"{META_BASE_URL}/loader/{gameVersion}", Defaults);
     }
 
     /// <summary>
@@ -54,7 +55,7 @@ public sealed class FabricApi {
     /// <param name="loaderVersion"></param>
     /// <returns></returns>
     public Task<FabricGameLoader> GetLoaderAsync(string gameVersion, string loaderVersion) {
-        return GetJsonAsync<FabricGameLoader>($"loader/{gameVersion}/{loaderVersion}");
+        return _httpClient.GetJsonAsync<FabricGameLoader>($"{META_BASE_URL}/loader/{gameVersion}/{loaderVersion}", Defaults);
     }
 
     /// <summary>
@@ -64,7 +65,7 @@ public sealed class FabricApi {
     /// <param name="loaderVersion"></param>
     /// <returns></returns>
     public Task<FabricGameLoader> GetLoaderZipAsync(string gameVersion, string loaderVersion) {
-        return GetJsonAsync<FabricGameLoader>($"loader/{gameVersion}/{loaderVersion}/profile/zip");
+        return _httpClient.GetJsonAsync<FabricGameLoader>($"{META_BASE_URL}/loader/{gameVersion}/{loaderVersion}/profile/zip", Defaults);
     }
 
     /// <summary>
@@ -73,8 +74,8 @@ public sealed class FabricApi {
     /// <param name="gameVersion"></param>
     /// <param name="loaderVersion"></param>
     /// <returns></returns>
-    public Task<FabricGameLoader> GetLoaderProfileAsync(string gameVersion, string loaderVersion) {
-        return GetJsonAsync<FabricGameLoader>($"loader/{gameVersion}/{loaderVersion}/profile/json");
+    public Task<FabricGameLoader>? GetLoaderProfileAsync(string gameVersion, string loaderVersion) {
+        return _httpClient.GetJsonAsync<FabricGameLoader>($"{META_BASE_URL}/loader/{gameVersion}/{loaderVersion}/profile/json", Defaults);
     }
 
     /// <summary>
@@ -84,7 +85,50 @@ public sealed class FabricApi {
     /// <param name="loaderVersion"></param>
     /// <returns></returns>
     public Task<FabricGameLoader> GetLoaderServerAsync(string gameVersion, string loaderVersion) {
-        return GetJsonAsync<FabricGameLoader>($"loader/{gameVersion}/{loaderVersion}/server/json");
+        return _httpClient.GetJsonAsync<FabricGameLoader>($"{META_BASE_URL}/loader/{gameVersion}/{loaderVersion}/server/json", Defaults);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="gameVersion"></param>
+    /// <param name="loaderVersion"></param>
+    /// <param name="installerVersion"></param>
+    /// <param name="savePath"></param>
+    public Task GetServerJarAsync(string gameVersion, string loaderVersion, string installerVersion,
+                                        string? savePath = default) {
+        return _httpClient.DownloadAsync(
+            GetDownloadUrl(gameVersion, loaderVersion, installerVersion),
+            savePath ?? GetServerFileName(gameVersion, loaderVersion, installerVersion));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="gameVersion"></param>
+    /// <param name="path"></param>
+    public async Task GetLatestJarAsync(string gameVersion, string? path = default) {
+        var installers = await GetInstallersAsync();
+        var loaders = await GetLoadersAsync(gameVersion);
+        await GetServerJarAsync(gameVersion,
+            loaders![0].Loader.Version,
+            installers![0].Version, path);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public Task<IReadOnlyList<FabricInstaller>?> GetInstallersAsync() {
+        return _httpClient.GetJsonAsync<IReadOnlyList<FabricInstaller>>($"{META_BASE_URL}/installer", Defaults);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public Task<IReadOnlyCollection<FabricGame>?> GetMinecraftVersionsAsync() {
+        return _httpClient.GetJsonAsync<IReadOnlyCollection<FabricGame>>($"{META_BASE_URL}/game", Defaults);
     }
 
     /// <summary>
@@ -107,78 +151,5 @@ public sealed class FabricApi {
     /// <returns></returns>
     public static string GetServerFileName(string gameVersion, string loaderVersion, string installerVersion) {
         return $"Fabric_{gameVersion}_{loaderVersion}_{installerVersion}.jar";
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="gameVersion"></param>
-    /// <param name="loaderVersion"></param>
-    /// <param name="installerVersion"></param>
-    /// <param name="savePath"></param>
-    public async Task GetServerJarAsync(string gameVersion, string loaderVersion, string installerVersion,
-                                        string? savePath = default) {
-        using var requestMessage = new HttpRequestMessage(HttpMethod.Get,
-            GetDownloadUrl(gameVersion, loaderVersion, installerVersion));
-        using var responseMessage = await _httpClient.SendAsync(requestMessage);
-
-        if (!responseMessage.IsSuccessStatusCode) {
-            _logger.LogError("Response code returned {Code} with reason: {Reason}",
-                responseMessage.StatusCode, responseMessage.ReasonPhrase);
-            throw new Exception(responseMessage.ReasonPhrase);
-        }
-
-        await using var fileStream =
-            File.OpenWrite(savePath ?? GetServerFileName(gameVersion, loaderVersion, installerVersion));
-        using var content = responseMessage.Content;
-        await content.CopyToAsync(fileStream);
-        await fileStream.FlushAsync();
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="gameVersion"></param>
-    /// <param name="path"></param>
-    public async Task GetLatestJarAsync(string gameVersion, string? path = default) {
-        var installers = await GetInstallersAsync();
-        var loaders = await GetLoadersAsync(gameVersion);
-        await GetServerJarAsync(gameVersion,
-            loaders![0].Loader.Version,
-            installers![0].Version, path);
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <returns></returns>
-    public Task<IReadOnlyList<FabricInstaller>?> GetInstallersAsync() {
-        return GetJsonAsync<IReadOnlyList<FabricInstaller>>("installer");
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <returns></returns>
-    public Task<IReadOnlyCollection<FabricGame>?> GetMinecraftVersionsAsync() {
-        return GetJsonAsync<IReadOnlyCollection<FabricGame>>("game");
-    }
-
-    private async Task<T?> GetJsonAsync<T>(string path) {
-        using var requestMessage = new HttpRequestMessage(HttpMethod.Get, $"{META_BASE_URL}/{path}");
-        using var responseMessage = await _httpClient.SendAsync(requestMessage);
-        if (!responseMessage.IsSuccessStatusCode) {
-            _logger.LogError("Response code returned {Code} with reason: {Reason}",
-                responseMessage.StatusCode, responseMessage.ReasonPhrase);
-            throw new Exception(responseMessage.ReasonPhrase);
-        }
-
-        using var content = responseMessage.Content;
-        await using var stream = await content.ReadAsStreamAsync();
-        if (stream.Length == 0 || !stream.CanRead) {
-            throw new Exception("Failed to read http content stream.");
-        }
-
-        return await JsonSerializer.DeserializeAsync<T>(stream, Defaults);
     }
 }
