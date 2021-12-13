1ceab79e1c866fa42824011b30653a328cf99317
Yucked <yucked@pm.me>
2021-12-13 00:03:46 -0500

    Finished FabricApi

--- Nylium.Minecraft/Providers/FabricProvider.cs
+++ Nylium.Minecraft/Providers/FabricProvider.cs
@@ -1,6 +1,4 @@
-using Microsoft.Extensions.Logging;
-using System.Text.Json;
-using System.Text.Json.Serialization;
+using Nylium.Fabric;
 
 // ReSharper disable UnusedAutoPropertyAccessor.Local
 
@@ -12,74 +10,41 @@ public sealed class FabricProvider : IServerProvider {
     public string Url
         => "https://meta.fabricmc.net/v2/versions";
 
-    private readonly HttpClient _httpClient;
-    private readonly ILogger<FabricProvider> _logger;
+    private readonly FabricApi _fabricApi;
 
     /// <summary>
     /// 
     /// </summary>
-    /// <param name="httpClient"></param>
-    /// <param name="logger"></param>
-    public FabricProvider(HttpClient httpClient, ILogger<FabricProvider> logger) {
-        _httpClient = httpClient;
-        _logger = logger;
+    /// <param name="fabricApi"></param>
+    public FabricProvider(FabricApi fabricApi) {
+        _fabricApi = fabricApi;
     }
 
     /// <inheritdoc />
     public async ValueTask<IEnumerable<Release>?> GetReleasesAsync() {
-        var versions = await GetMinecraftVersions();
+        var versions = await _fabricApi.GetMinecraftVersionsAsync();
+        var installer = (await _fabricApi.GetInstallersAsync())![0];
+        var releases = new HashSet<Release>();
 
-        return default;
-    }
+        await Parallel.ForEachAsync(versions!.Where(x => x.IsStable).Take(5),
+            async (game, _) => {
+                var loaders = await _fabricApi.GetLoadersAsync(game.Version);
+                Parallel.ForEach(loaders!.Where(x => x.Loader.IsStable)
+                        .Take(10)
+                        .Select(x => x.Loader),
+                    (loader, _) => {
+                        releases.Add(new Release {
+                            Version = new Version {
+                                Minecraft = game.Version,
+                                IsPrerelease = game.IsStable,
+                                Loader = loader.Version
+                            },
+                            ReleasedOn = default,
+                            DownloadUrl = FabricApi.GetDownloadUrl(game.Version, loader.Version, installer.Version)
+                        });
+                    });
+            });
 
-    private async Task<IEnumerable<Version>> GetMinecraftVersions() {
-        await using var stream = await GetStreamAsync("game");
-        if (stream == null || stream.Length == 0) {
-            _logger.LogError("Fetching minecraft versions failed!");
-            return default!;
-        }
-
-        using var document = await JsonDocument.ParseAsync(stream);
-        return document.RootElement.EnumerateArray()
-            .Select(x => new Version {
-                Minecraft = x.GetProperty("version").GetString()!,
-                IsPrerelease = x.GetProperty("stable").GetBoolean()
-            })!;
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="version"></param>
-    public async Task GetVersionLoaders(string version) {
-        await using var stream = await GetStreamAsync($"loader/{version}");
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <returns></returns>
-    public async Task<IEnumerable<string>> GetInstallers() {
-        await using var stream = await GetStreamAsync("installer");
-        if (stream == null || stream.Length == 0) {
-            _logger.LogError("Fetching minecraft versions failed!");
-            return default!;
-        }
-
-        using var document = await JsonDocument.ParseAsync(stream);
-        return document.RootElement.EnumerateArray()
-            .Select(x => x.GetProperty("version").GetString()!);
-    }
-
-    private async Task<Stream?> GetStreamAsync(string path) {
-        using var requestMessage = new HttpRequestMessage(HttpMethod.Get, $"{Url}/{path}");
-        using var responseMessage = await _httpClient.SendAsync(requestMessage);
-        if (!responseMessage.IsSuccessStatusCode) {
-            _logger.LogError("{ReasonPhrase}", responseMessage.ReasonPhrase);
-            return default;
-        }
-
-        using var content = responseMessage.Content;
-        return await content.ReadAsStreamAsync();
+        return releases;
     }
 }
