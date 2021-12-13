258353ec72e89deace088dc13020e94d04bae436
Yucked <yucked@pm.me>
2021-12-13 16:57:54 -0500

    finished paper wrapper

--- Nylium.Minecraft/Providers/PaperProvider.cs
+++ Nylium.Minecraft/Providers/PaperProvider.cs
@@ -1,3 +1,5 @@
+using Nylium.Common;
+
 namespace Nylium.Minecraft.Providers;
 
 /// <inheritdoc />
@@ -9,11 +11,72 @@ public sealed class PaperProvider : IServerProvider {
     /// <summary>
     /// 
     /// </summary>
-    public static string LegacyBuilds
-        => "https://papermc.io/legacy#Paper";
+    public const string API_URL
+        = "https://papermc.io/api/v2/projects/paper/";
+
+    private readonly HttpClient _httpClient;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="httpClient"></param>
+    public PaperProvider(HttpClient httpClient) {
+        _httpClient = httpClient;
+    }
 
     /// <inheritdoc />
-    public ValueTask<IEnumerable<Release>?> GetReleasesAsync() {
-        throw new NotImplementedException();
+    public async ValueTask<IEnumerable<Release>?> GetReleasesAsync() {
+        var releases = new List<Release>();
+        var versions = await GetVersionsAsync();
+        await Parallel.ForEachAsync(versions, async (version, _) => {
+
+            var builds = await GetBuildsAsync(version);
+            await Parallel.ForEachAsync(builds, async (build, _) => {
+                var baseUrl = $"{API_URL}/versions/{version}/builds/{build}";
+                var element = await _httpClient.GetJsonAsync(baseUrl);
+                var downloadName = element
+                    .GetProperty("downloads")
+                    .GetProperty("application")
+                    .GetProperty("name")
+                    .GetString();
+
+                releases.Add(new Release {
+                    Version = new Version {
+                        Minecraft = version,
+                        IsPrerelease = true,
+                        Loader = $"{build}"
+                    },
+                    DownloadUrl = (await GetDownloadUrlAsync(version, build)).Url,
+                    ReleasedOn = default
+                });
+            });
+        });
+        return releases;
+    }
+
+    private async Task<(string Url, string FileName)> GetDownloadUrlAsync(string version, int build) {
+        var baseUrl = $"{API_URL}/versions/{version}/builds/{build}";
+        var element = await _httpClient.GetJsonAsync(baseUrl);
+        var downloadName = element
+            .GetProperty("downloads")
+            .GetProperty("application")
+            .GetProperty("name")
+            .GetString()!;
+
+        return ($"{baseUrl}/{build}/downloads/{downloadName}", downloadName);
+    }
+
+    private async Task<IEnumerable<int>> GetBuildsAsync(string version) {
+        var element = await _httpClient.GetJsonAsync($"{API_URL}/versions/{version}");
+        return element.GetProperty("builds")
+            .EnumerateArray()
+            .Select(x => x.GetInt32());
+    }
+
+    private async Task<IEnumerable<string>> GetVersionsAsync() {
+        var element = await _httpClient.GetJsonAsync(API_URL);
+        return element.GetProperty("versions")
+            .EnumerateArray()
+            .Select(x => x.GetString()!);
     }
 }
